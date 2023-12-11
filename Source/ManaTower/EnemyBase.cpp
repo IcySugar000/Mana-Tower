8c1f46b1cea9bac582f7dcbfa6f1710762c677f1
JLQusername <1841129348@qq.com>
2023-12-12 01:48:33 +0800

    EnemyBase的更新
    太好啦，我也上传代码啦！
    补充了速度，攻击力，防御力，对一些函数进行了修改，对一些变量的类型进行了修改。

--- Source/ManaTower/EnemyBase.cpp
+++ Source/ManaTower/EnemyBase.cpp
@@ -3,18 +3,21 @@
 
 #include "EnemyBase.h"
 
-AEnemyBase::AEnemyBase() {
+AEnemyBase::AEnemyBase() 
+{
 	FlipbookLibrary.FindOrAdd(FString("Idle"));
 	FlipbookLibrary.FindOrAdd(FString("Move"));
 
-    if (MaxHealth <= 0) MaxHealth = 1;
-	Health = MaxHealth;
-    IsDead = false;
+    IsDead = false;//活着的怪物
 }
 
 void AEnemyBase::BeginPlay()
 {
     Super::BeginPlay();
+
+    if (MaxHealth <= 0)
+        MaxHealth = 80;//如果最大生命值出现设置错误，那么就改成80
+    Health = MaxHealth;//生命初值为最大生命值
 }
 
 void AEnemyBase::Tick(float DeltaSeconds) {
@@ -32,7 +35,6 @@ float AEnemyBase::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent
     Health -= DamageAmount;
     if(Health <= 0 && !IsDead) {
         Health = 0;
-        IsDead = true;
         Die();
     }
     return 0.0f;
@@ -57,5 +59,6 @@ void AEnemyBase::UpdateFlipbook()
 }
 
 void AEnemyBase::Die() {
-    UE_LOG(LogTemp, Warning, TEXT("AHHHH. I DIED"))
+    IsDead = true;
+    UE_LOG(LogTemp, Warning, TEXT("AHHHH. I DIED"));
 }
