4494169b8c13b83f832ffafdd91361cd61b3da9b
GGssq <1104807401@qq.com>
2023-12-29 14:48:05 +0800

    增加了雕像功能

---
+++ Source/ManaTower/StatueRoom.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RoomBase.h"
+#include "StatueRoom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MANATOWER_API AStatueRoom : public ARoomBase
+{
+	GENERATED_BODY()
+	
+};
